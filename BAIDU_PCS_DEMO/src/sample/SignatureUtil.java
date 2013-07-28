cd8a01aebcf81ae683d585866b76669e5a885133
yongbo <821018134@qq.com>
2013-07-28 21:47:27 +0800

    第一个版本

---
+++ BAIDU_PCS_DEMO/src/sample/SignatureUtil.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.net.URLEncoder;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class SignatureUtil {
+	static String secretKey = "116dfc831de5298f73db6d7dadf611b9";
+	private final static String Flag = "MBO";
+	private final static String content = Flag + "\n"
+	          + "Method=GET" + "\n"
+	          + "Bucket=%1$s" + "\n"
+	          + "Object=/%2$s" + "\n";
+	public static String createSignature(String bucket,String savePath) {
+		Mac mac;
+		String out = null;
+		String txt = String.format(content, bucket, savePath);
+		try {
+			mac = Mac.getInstance("HmacSHA1");
+			SecretKeySpec secret = new SecretKeySpec(secretKey.getBytes(),"HmacSHA1");
+		    mac.init(secret);
+		    out = new String(Base64.encodeBase64(mac.doFinal(txt.getBytes())));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(out == null){return null;}
+		return URLEncoder.encode(out);//urlencode(base64_encode(hash_hmac("hash1", Content, secretKey,true)));
+	}
+}
